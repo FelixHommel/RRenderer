32b4e3dbdc51ef7a618aba490de3074fa6c1924c
lpu06 <felix.hommel@uni-ulm.de>
2025-05-25 01:52:05 +0200

    feat: add minimal working example to main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,21 @@
+#include "VulkanRenderer.hpp"
+#include "window/Window.hpp"
+
+#include <string>
+
+static constexpr int WINDOW_WIDTH{700};
+static constexpr int WINDOW_HEIGHT{700};
+static constexpr std::string WINDOW_TITLE{"title"};
+
 int main()
 {
+    rr::Window w{WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE};
+    rr::VulkanRenderer r{w};
+
+    while(w.shouldClose() == 0)
+    {
+        glfwPollEvents();
+    }
+
     return 0;
 }
