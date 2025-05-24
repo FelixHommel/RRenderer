cd194e8f0819a1d6d3f5bc3a160b8c5926fb01e2
lpu06 <felix.hommel@uni-ulm.de>
2025-05-25 01:49:36 +0200

    feat: add GLFWwindow wrapper

---
+++ src/engine/window/Window.cpp
@@ -0,0 +1,46 @@
+#include "Window.hpp"
+
+#include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h>
+
+#include <stdexcept>
+#include <string>
+
+namespace rr
+{
+
+Window::Window(int width, int height, const std::string& title)
+    : m_window(nullptr)
+    , m_width(width)
+    , m_height(height)
+    , m_title(title)
+{
+    if(glfwInit() == 0)
+        throw std::runtime_error("ERROR: Failed to initialize GLFW");
+
+    spdlog::info("GLFW initialized successfully...");
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+    if(m_window == nullptr)
+        throw std::runtime_error("ERROR: Failed to create a GLFW window");
+
+    spdlog::info("Window({}, {}) created successfully...", m_width, m_height);
+}
+
+Window::~Window()
+{
+    spdlog::info("Shutting down...");
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+}
+
+void Window::createWindowSurface(VkInstance& instance, VkSurfaceKHR* surface)
+{
+    if(glfwCreateWindowSurface(instance, m_window, nullptr, surface) != VK_SUCCESS)
+        throw std::runtime_error("GLFW failed to create window surface");
+}
+
+} // !rr
