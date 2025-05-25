91443862029d21c1501c62cf24ed06ed371cf651
lpu06 <felix.hommel@uni-ulm.de>
2025-05-25 23:28:51 +0200

    feat: add VulkanPipelineLayout class

---
+++ src/engine/core/VulkanPipelineLayout.cpp
@@ -0,0 +1,35 @@
+#include "VulkanPipelineLayout.hpp"
+#include "spdlog/spdlog.h"
+#include <stdexcept>
+#include <vulkan/vulkan_core.h>
+
+namespace rr
+{
+
+VulkanPipelineLayout::VulkanPipelineLayout(VkDevice device)
+    : device(device)
+    , m_pipelineLayout(VK_NULL_HANDLE)
+{
+    VkPipelineLayoutCreateInfo createInfo{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .setLayoutCount = 0,
+        .pSetLayouts = nullptr,
+        .pushConstantRangeCount = 0,
+        .pPushConstantRanges = nullptr
+    };
+
+    if(vkCreatePipelineLayout(device, &createInfo, nullptr, &m_pipelineLayout) != VK_SUCCESS)
+    {
+        spdlog::critical("Failure while creaing pipeline layout");
+        throw std::runtime_error("Failed to create pipeline layout");
+    }
+
+    spdlog::info("Created pipeline layout successfully...");
+}
+
+VulkanPipelineLayout::~VulkanPipelineLayout()
+{
+    vkDestroyPipelineLayout(device,  m_pipelineLayout, nullptr);
+}
+
+} // !rr
---
+++ src/engine/core/VulkanPipelineLayout.hpp
@@ -0,0 +1,30 @@
+#ifndef RRENDERER_ENGINE_CORE_VULKAN_PIPELINE_LAYOUT_HPP
+#define RRENDERER_ENGINE_CORE_VULKAN_PIPELINE_LAYOUT_HPP
+
+#include <vulkan/vulkan_core.h>
+
+namespace rr
+{
+
+class VulkanPipelineLayout
+{
+public:
+    VulkanPipelineLayout(VkDevice device);
+    ~VulkanPipelineLayout();
+
+    VulkanPipelineLayout(const VulkanPipelineLayout&) = delete;
+    VulkanPipelineLayout(VulkanPipelineLayout&&) = delete;
+    VulkanPipelineLayout& operator=(const VulkanPipelineLayout&) = delete;
+    VulkanPipelineLayout& operator=(VulkanPipelineLayout&&) = delete;
+
+    [[nodiscard]] VkPipelineLayout getHandle() const { return m_pipelineLayout; }
+
+private:
+    VkDevice device;
+
+    VkPipelineLayout m_pipelineLayout;
+};
+
+} // !rr
+
+#endif // !RRENDERER_ENGINE_CORE_VULKAN_PIPELINE_LAYOUT_HPP
