b9702fabb412a73d73ce0180ad466a454ed58b74
lpu06 <felix.hommel@uni-ulm.de>
2025-05-26 14:31:43 +0200

    refactor: remove bind from interface

--- src/engine/core/interfaces/IPipeline.hpp
+++ src/engine/core/interfaces/IPipeline.hpp
@@ -1,8 +1,6 @@
 #ifndef RRENDERER_ENGINE_CORE_INTERFACES_IPIPELINE_HPP
 #define RRENDERER_ENGINE_CORE_INTERFACES_IPIPELINE_HPP
 
-#include "core/CommandBuffer.hpp"
-
 namespace rr
 {
 
@@ -16,8 +14,6 @@ public:
     IPipeline(IPipeline&&) = delete;
     IPipeline& operator=(const IPipeline&) = delete;
     IPipeline& operator=(IPipeline&&) = delete;
-
-    virtual void bind(CommandBuffer& cmdBuffer) = 0;
 };
 
 } // !rr
