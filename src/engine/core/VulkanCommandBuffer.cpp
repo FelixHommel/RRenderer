deb7f4d4ea7135688cda9c8ddbbdf11479f55902
lpu06 <felix.hommel@uni-ulm.de>
2025-05-26 14:30:20 +0200

    feat: add CommandBuffer wrapper

---
+++ src/engine/core/VulkanCommandBuffer.cpp
@@ -0,0 +1,74 @@
+#include "VulkanCommandBuffer.hpp"
+
+#include "core/interfaces/ICommandBuffer.hpp"
+#include "spdlog/spdlog.h"
+#include <vulkan/vulkan_core.h>
+
+#include <memory>
+#include <stdexcept>
+#include <vector>
+
+namespace rr
+{
+
+/**
+ *  Construct a single CommandBuffer
+ */
+VulkanCommandBuffer::VulkanCommandBuffer(VkDevice device, VkCommandPool commandPool, VkCommandBufferLevel level)
+    : m_commandBuffer(VK_NULL_HANDLE)
+{
+    VkCommandBufferAllocateInfo allocInfo{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .commandPool = commandPool,
+        .level = level,
+        .commandBufferCount = 1
+    };
+
+    if(vkAllocateCommandBuffers(device, &allocInfo, &m_commandBuffer) != VK_SUCCESS)
+    {
+        spdlog::critical("Failure while creating command buffers");
+        throw std::runtime_error("Failed to create command buffers");
+    }
+}
+
+/**
+ *  Helper constructor for the create function
+ */
+VulkanCommandBuffer::VulkanCommandBuffer(VkCommandBuffer commandBuffer)
+    : m_commandBuffer(commandBuffer)
+{}
+
+/**
+ * Factory method to create <code>count<\code> of <code>VulkanCommandBuffer<\code>
+ *
+ * @param device - VkDevice from where the buffers are located on
+ * @param commandPool - VkCommandPool where the buffers are allocated in
+ * @param cound - amount of buffers to allocate
+ * @param level - level of the buffers. Default: VK_COMMAND_BUFFER_LEVEL_PRIMARY
+ * @return vector with the allocated VulkanCommandBuffers
+*/
+std::vector<std::unique_ptr<VulkanCommandBuffer>> VulkanCommandBuffer::create(VkDevice device, VkCommandPool commandPool, std::uint32_t count, VkCommandBufferLevel level)
+{
+    std::vector<VkCommandBuffer> rawBuffers(count);
+
+    VkCommandBufferAllocateInfo allocInfo{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .commandPool = commandPool,
+        .level = level,
+        .commandBufferCount = count
+    };
+
+    if(vkAllocateCommandBuffers(device, &allocInfo, rawBuffers.data()) != VK_SUCCESS)
+    {
+        spdlog::critical("Failure while creating command buffers");
+        throw std::runtime_error("Failed to create command buffers");
+    }
+
+    std::vector<std::unique_ptr<VulkanCommandBuffer>> wrapped;
+    for(const auto& buffer : rawBuffers)
+        wrapped.push_back(std::make_unique<VulkanCommandBuffer>(buffer));
+
+    return wrapped;
+}
+
+} // !rr
---
+++ src/engine/core/VulkanCommandBuffer.hpp
@@ -0,0 +1,42 @@
+#ifndef RRENDERER_ENGINE_CORE_VULKAN_COMMAND_BUFFER_HPP
+#define RRENDERER_ENGINE_CORE_VULKAN_COMMAND_BUFFER_HPP
+
+#include "core/interfaces/ICommandBuffer.hpp"
+
+#include <vulkan/vulkan_core.h>
+
+#include <memory>
+#include <vector>
+
+namespace rr
+{
+
+/**
+ *  <code>VulkanCommandBuffer<\code> is a wrapper around VkCommandBuffer.
+ *
+ *  @author Felix Hommel
+ *  @date 5/26/2025
+*/
+class VulkanCommandBuffer : public ICommandBuffer
+{
+public:
+    VulkanCommandBuffer(VkDevice device, VkCommandPool commandPool, VkCommandBufferLevel = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+    VulkanCommandBuffer(VkCommandBuffer commandBuffer);
+    ~VulkanCommandBuffer() override = default;
+
+    VulkanCommandBuffer(const VulkanCommandBuffer&) = delete;
+    VulkanCommandBuffer(VulkanCommandBuffer&&) = delete;
+    VulkanCommandBuffer& operator=(const VulkanCommandBuffer&) = delete;
+    VulkanCommandBuffer& operator=(VulkanCommandBuffer&&) = delete;
+
+    static std::vector<std::unique_ptr<VulkanCommandBuffer>> create(VkDevice device, VkCommandPool commandPool, std::uint32_t count, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+
+    [[nodiscard]] VkCommandBuffer getHandle() const { return m_commandBuffer; }
+    
+private:
+    VkCommandBuffer m_commandBuffer;
+};
+
+} // !rr
+
+#endif // !RRENDERER_ENGINE_CORE_VULKAN_COMMAND_BUFFER_HPP
