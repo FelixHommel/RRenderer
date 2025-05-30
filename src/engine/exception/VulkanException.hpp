017c95aa49251c74a7228e76eeab121d12053cab
lpu06 <felix.hommel@uni-ulm.de>
2025-05-30 20:25:04 +0200

    refactor: add causeToString as member functions

--- src/engine/exception/VulkanException.hpp
+++ src/engine/exception/VulkanException.hpp
@@ -16,38 +16,21 @@ enum class VulkanExceptionCause
     SUBMIT_COMMAND_BUFFER,
     BEGIN_RECORD_COMMAND_BUFFER,
     END_RECORD_COMMAND_BUFFER,
-    ALLOCATE_COMMAND_BUFFERS
+    ALLOCATE_COMMAND_BUFFERS,
+    QUEUE_FAMILY_INDEX_IS_EMPTY,
+    CREATE_COMMAND_POOL
 };
-
-std::string toString(VulkanExceptionCause cause)
-{
-    switch(cause)
-    {
-        case VulkanExceptionCause::CREATE_INSTANCE: return "instance creation";
-        case VulkanExceptionCause::SUBMIT_COMMAND_BUFFER: return "command buffer submission";
-        case VulkanExceptionCause::IMAGE_ACQUISITION: return "the acquisition of the next image";
-        case VulkanExceptionCause::BEGIN_RECORD_COMMAND_BUFFER: return "begin recording of command buffer #";
-        case VulkanExceptionCause::END_RECORD_COMMAND_BUFFER: return "ending of recording command buffer #";
-        case VulkanExceptionCause::ALLOCATE_COMMAND_BUFFERS: return " allocating command buffers";
-        default: return " unknown events";
-    }
-}
-
-std::string toString(VulkanExceptionCause cause, std::size_t index)
-{
-    return toString(cause) + std::to_string(index);
-}
 
 class VulkanException : public EngineException
 {
 public:
     VulkanException(VulkanExceptionCause cause)
-        : EngineException("Vulkan error occurd during " + toString(cause))
+        : EngineException("Vulkan error occurd during " + causeToString(cause))
         , m_cause(cause)
     {}
 
     VulkanException(VulkanExceptionCause cause, std::size_t index)
-        : EngineException("Vulkan error occurd during " + toString(cause, index))
+        : EngineException("Vulkan error occurd during " + causeToString(cause, index))
         , m_cause(cause)
     {}
 
@@ -55,6 +38,27 @@ public:
 
 private:
     VulkanExceptionCause m_cause;
+
+    std::string causeToString(VulkanExceptionCause cause)
+    {
+        switch(cause)
+        {
+            case VulkanExceptionCause::CREATE_INSTANCE: return "creation of an instance";
+            case VulkanExceptionCause::SUBMIT_COMMAND_BUFFER: return "command buffer submission";
+            case VulkanExceptionCause::IMAGE_ACQUISITION: return "the acquisition of the next image";
+            case VulkanExceptionCause::BEGIN_RECORD_COMMAND_BUFFER: return "begin recording of command buffer #";
+            case VulkanExceptionCause::END_RECORD_COMMAND_BUFFER: return "ending of recording command buffer #";
+            case VulkanExceptionCause::ALLOCATE_COMMAND_BUFFERS: return "allocating command buffers";
+            case VulkanExceptionCause::QUEUE_FAMILY_INDEX_IS_EMPTY: return "trying to get the index of a non-existing queue";
+            case VulkanExceptionCause::CREATE_COMMAND_POOL: return "creation of a command pool";
+            default: return "unknown events";
+        }
+    }
+
+    std::string causeToString(VulkanExceptionCause cause, std::size_t index)
+    {
+        return causeToString(cause) + std::to_string(index);
+    }
 };
 
 } // !rr
