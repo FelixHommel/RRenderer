6352584d743e4396f1ca212812e698af1ffec033
lpu06 <felix.hommel@uni-ulm.de>
2025-06-01 22:26:19 +0200

    refactor: use custom exceptions

--- src/engine/window/Window.cpp
+++ src/engine/window/Window.cpp
@@ -1,10 +1,13 @@
 #include "Window.hpp"
 
+#include "exception/EngineException.hpp"
+#include "exception/GLFWException.hpp"
+
 #include "GLFW/glfw3.h"
 #include <spdlog/spdlog.h>
 #include <vulkan/vulkan_core.h>
 
-#include <stdexcept>
+#include <source_location>
 #include <string>
 
 namespace rr
@@ -17,7 +20,7 @@ Window::Window(int width, int height, const std::string& title)
     , m_title(title)
 {
     if(glfwInit() == 0)
-        throw std::runtime_error("ERROR: Failed to initialize GLFW");
+        throwWithLog<GLFWException>(std::source_location::current(), GLFWExceptionCause::GLFW_INIT_FAILED);
 
     spdlog::info("GLFW initialized successfully...");
 
@@ -26,21 +29,20 @@ Window::Window(int width, int height, const std::string& title)
 
     m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
     if(m_window == nullptr)
-        throw std::runtime_error("ERROR: Failed to create a GLFW window");
+        throwWithLog<GLFWException>(std::source_location::current(), GLFWExceptionCause::WINDOW_CREATION_FAILED);
 
     spdlog::info("Window({}, {}) created successfully...", m_width, m_height);
 }
 
 Window::~Window()
 {
-    spdlog::info("Shutting down...");
     glfwDestroyWindow(m_window);
 }
 
 void Window::createWindowSurface(VkInstance& instance, VkSurfaceKHR* surface)
 {
     if(glfwCreateWindowSurface(instance, m_window, nullptr, surface) != VK_SUCCESS)
-        throw std::runtime_error("GLFW failed to create window surface");
+        throwWithLog<GLFWException>(std::source_location::current(), GLFWExceptionCause::SURFACE_CREATION_FAILED);
 }
 
 } // !rr
