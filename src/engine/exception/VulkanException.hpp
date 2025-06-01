aebbe254e5c008343c6259f32ee04c1ff01f67d4
lpu06 <felix.hommel@uni-ulm.de>
2025-06-01 22:19:06 +0200

    refactor: mark constructor explicit and add using enum VulkanExceptionCause

--- src/engine/exception/VulkanException.hpp
+++ src/engine/exception/VulkanException.hpp
@@ -46,7 +46,7 @@ enum class VulkanExceptionCause : std::uint8_t
 class VulkanException : public EngineException
 {
 public:
-    VulkanException(VulkanExceptionCause cause)
+    explicit VulkanException(VulkanExceptionCause cause)
         : EngineException("Vulkan error occurd during " + causeToString(cause))
         , m_cause(cause)
     {}
@@ -65,35 +65,37 @@ private:
     {
         switch(cause)
         {
-            case VulkanExceptionCause::CREATE_INSTANCE: return "creation of a VkInstance";
-            case VulkanExceptionCause::SUBMIT_COMMAND_BUFFER: return "submission of a VkCommandBuffer";
-            case VulkanExceptionCause::IMAGE_ACQUISITION: return "the acquisition of the next swapchain image";
-            case VulkanExceptionCause::BEGIN_RECORD_COMMAND_BUFFER: return "begin recording of VkCommandBuffer #";
-            case VulkanExceptionCause::END_RECORD_COMMAND_BUFFER: return "ending recording of VkCommandBuffer #";
-            case VulkanExceptionCause::ALLOCATE_COMMAND_BUFFERS: return "allocating VkCommandBuffers";
-            case VulkanExceptionCause::QUEUE_FAMILY_INDEX_IS_EMPTY: return "trying to get the index of a non-existing VkQueue";
-            case VulkanExceptionCause::CREATE_COMMAND_POOL: return "creation of a VkCommandPool";
-            case VulkanExceptionCause::CREATE_DEBUG_MESSENGER: return "creation of the VkDebugMessenger";
-            case VulkanExceptionCause::FIND_SUPPORTED_FORMAT: return "search for a supported VkFormat";
-            case VulkanExceptionCause::CREATE_IMAGE: return "creation of an VkImage";
-            case VulkanExceptionCause::ALLOCATE_MEMORY: return "allocating memory";
-            case VulkanExceptionCause::BIND_IMAGE_MEMORY: return "binding image memory";
-            case VulkanExceptionCause::NO_PHYSICAL_DEVICE_FOUND: return "search for vulkan compatible VkPhysicalDevice";
-            case VulkanExceptionCause::NO_SUITABLE_DEVICE_FOUND: return "search for suitable VkPhysicalDevice";
-            case VulkanExceptionCause::CREATE_DEVICE: return "creation of a VkDevice";
-            case VulkanExceptionCause::NO_SUITABLE_MEMORY_TYPE_FOUND: return "search for a suitable memory type";
-            case VulkanExceptionCause::GLFW_EXTENSIONS_MISSING: return "verifying availability of extensions required by GLFW";
-            case VulkanExceptionCause::CREATE_GRAPHICS_PIPELINE: return "creation of VkPipeline";
-            case VulkanExceptionCause::CREATE_SHADER_MODULE: return "creation of VkShaderModule";
-            case VulkanExceptionCause::CREATE_PIPELINE_LAYOUT: return "creation of VkPipelineLayout";
-            case VulkanExceptionCause::QUEUE_SUBMIT_GRAPHICS: return "submiting graphics queue";
-            case VulkanExceptionCause::CREATE_SWAPCHAIN_KHR: return "creation of VkSwapchain";
-            case VulkanExceptionCause::CREATE_IMAGE_VIEW: return "creation of VkImageView #";
-            case VulkanExceptionCause::CREATE_RENDER_PASS: return "creation of VkRenderPass";
-            case VulkanExceptionCause::CREATE_FRAMEBUFFER: return "creation of framebuffer #";
-            case VulkanExceptionCause::CREATE_SEMAPHORE: return "creation of VkSemaphore #";
-            case VulkanExceptionCause::CREATE_IN_FLIGHT_SYNC_OBJECT: return "creation of in flight VkFence or VkSemaphore #";
-            case VulkanExceptionCause::VALIDATION_LAYERS_UNAVAILABLE: return "checking for validation layer availability";
+            using enum VulkanExceptionCause;
+
+            case CREATE_INSTANCE: return "creation of a VkInstance";
+            case SUBMIT_COMMAND_BUFFER: return "submission of a VkCommandBuffer";
+            case IMAGE_ACQUISITION: return "the acquisition of the next swapchain image";
+            case BEGIN_RECORD_COMMAND_BUFFER: return "begin recording of VkCommandBuffer #";
+            case END_RECORD_COMMAND_BUFFER: return "ending recording of VkCommandBuffer #";
+            case ALLOCATE_COMMAND_BUFFERS: return "allocating VkCommandBuffers";
+            case QUEUE_FAMILY_INDEX_IS_EMPTY: return "trying to get the index of a non-existing VkQueue";
+            case CREATE_COMMAND_POOL: return "creation of a VkCommandPool";
+            case CREATE_DEBUG_MESSENGER: return "creation of the VkDebugMessenger";
+            case FIND_SUPPORTED_FORMAT: return "search for a supported VkFormat";
+            case CREATE_IMAGE: return "creation of an VkImage";
+            case ALLOCATE_MEMORY: return "allocating memory";
+            case BIND_IMAGE_MEMORY: return "binding image memory";
+            case NO_PHYSICAL_DEVICE_FOUND: return "search for vulkan compatible VkPhysicalDevice";
+            case NO_SUITABLE_DEVICE_FOUND: return "search for suitable VkPhysicalDevice";
+            case CREATE_DEVICE: return "creation of a VkDevice";
+            case NO_SUITABLE_MEMORY_TYPE_FOUND: return "search for a suitable memory type";
+            case GLFW_EXTENSIONS_MISSING: return "verifying availability of extensions required by GLFW";
+            case CREATE_GRAPHICS_PIPELINE: return "creation of VkPipeline";
+            case CREATE_SHADER_MODULE: return "creation of VkShaderModule";
+            case CREATE_PIPELINE_LAYOUT: return "creation of VkPipelineLayout";
+            case QUEUE_SUBMIT_GRAPHICS: return "submiting graphics queue";
+            case CREATE_SWAPCHAIN_KHR: return "creation of VkSwapchain";
+            case CREATE_IMAGE_VIEW: return "creation of VkImageView #";
+            case CREATE_RENDER_PASS: return "creation of VkRenderPass";
+            case CREATE_FRAMEBUFFER: return "creation of framebuffer #";
+            case CREATE_SEMAPHORE: return "creation of VkSemaphore #";
+            case CREATE_IN_FLIGHT_SYNC_OBJECT: return "creation of in flight VkFence or VkSemaphore #";
+            case VALIDATION_LAYERS_UNAVAILABLE: return "checking for validation layer availability";
             default: return "unknown events";
         }
     }
